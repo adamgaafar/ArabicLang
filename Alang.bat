6fdf46d96b884cf2ec7ae492cc347ebd89d985c3
Adam Gaafar <dodogaafar.ag@gmail.com>
2026-03-13 00:37:08 +0200

    Updated all the files to be globally working

--- Alang.bat
+++ Alang.bat
@@ -1,3 +1,3 @@
 @echo off
 chcp 65001 > nul
-java -cp "C:\Users\adamgaafar\Documents\Projects\Alang\target\classes;C:\Users\adamgaafar\.m2\repository\org\antlr\antlr4-runtime\4.13.2\antlr4-runtime-4.13.2.jar" ArabicLangCLI %*
+java -jar "%~dp0target\alang-cli.jar" %*
